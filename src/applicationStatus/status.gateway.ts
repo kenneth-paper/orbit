c5056627a09167b7ee23971f9f60444ded8d6871
andre-tirta <andretirtawijaya@gmail.com>
2019-10-25 18:44:38 +0700

    add https and set to trigger emit status on connection established if value from database is 0

--- src/applicationStatus/status.gateway.ts
+++ src/applicationStatus/status.gateway.ts
@@ -1,11 +1,24 @@
-import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection } from "@nestjs/websockets";
 import { Server } from "socket.io";
-import { Logger } from "@nestjs/common";
+import { Logger, Injectable, Inject, forwardRef } from "@nestjs/common";
+import { ApplicationStatusService } from "./applicationStatus.service";
 
 @WebSocketGateway()
-export class StatusGateway {
+@Injectable()
+export class StatusGateway implements OnGatewayConnection {
+
+  constructor(@Inject(forwardRef(()=>ApplicationStatusService))private service: ApplicationStatusService) {}
+
   @WebSocketServer() wss: Server;
   private logger: Logger = new Logger('StatusGateway');
+
+  async handleConnection(client: any, ...args: any[]) {
+    var result = await this.service.getApplicationStatus();
+    if (!result.status){
+      this.sendToAll('status');
+    }
+  }
+
   sendToAll(msg: string){
     this.logger.log(`StatusGateway -> sendToAll ${msg}`);
     this.wss.emit(msg);
