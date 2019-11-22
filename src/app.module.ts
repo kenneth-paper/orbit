d026e1561afb0acd5dab9e5e37d079aa7e510535
andretirtawijaya <andretirtawijaya@gmail.com>
2019-11-22 18:37:41 +0700

    fix relative path to database.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ApplicationStatusModule } from './applicationStatus/applicationStatus.module';
-import { DatabaseModule } from 'environment/database.module';
+import { DatabaseModule } from '../environment/database.module';
 
 @Module({
   imports: [DatabaseModule, ApplicationStatusModule],
