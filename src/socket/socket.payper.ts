1f248ab93a2302a79d1f957d3f1d75e4eff706fb
Kenneth Brice <kenneth.brice@ti.ukdw.ac.id>
2020-08-06 18:12:03 +0700

    testing socket

---
+++ src/socket/socket.payper.ts
@@ -0,0 +1,45 @@
+import {
+    SubscribeMessage,
+    WebSocketGateway,
+    OnGatewayInit,
+    WebSocketServer,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+   } from '@nestjs/websockets';
+  import { Logger } from '@nestjs/common';
+  import { Socket, Server } from 'socket.io';
+  
+   @WebSocketGateway({namespace: "/socket/payper"})
+   export class SocketPayper implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+   
+    @WebSocketServer() server: Server;
+    private logger: Logger = new Logger('AppSocketPayper');
+   
+    // @UseGuards(SocketGuard)
+    @SubscribeMessage('join')
+    joinRoom(client: Socket, room: string) {
+      client.join(room);
+      return room;
+    }
+  
+    // @UseGuards(InternalSocketGuard)
+    @SubscribeMessage('paymentStatus')
+    paymentStatus(client: Socket, payload: any): void {
+      console.log(payload.progress)
+      this.server.in(payload.room).emit('paymentStatus', payload);
+    }
+   
+    afterInit(server: Server) {
+  
+    }
+   
+    handleDisconnect(client: Socket) {
+     this.logger.log(`Client disconnected: ${client.id}`);
+    }
+   
+    handleConnection(client: Socket, ...args: any[]) {
+     this.logger.log(`Client connected: ${client.id}`);
+    }
+   }
+  
+  
