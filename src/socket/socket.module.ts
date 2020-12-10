5d84c1684085cb3dbd3d6871144e1c78929fe220
Arnold <noldwidjaja@gmail.com>
2020-12-10 20:41:38 +0700

    init paperchain socket

--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -1,5 +1,6 @@
 import { Module, HttpModule } from '@nestjs/common';
 import { SocketGateway } from './socket.gateway';
+import { SocketPaperChain } from './socket.paperchain';
 import { SocketPayper } from './socket.payper';
 
 @Module({
@@ -12,7 +13,8 @@ import { SocketPayper } from './socket.payper';
       ],
       exports: [
         SocketGateway,
-        SocketPayper
+        SocketPayper,
+        SocketPaperChain
       ],
       controllers: [],
 })
