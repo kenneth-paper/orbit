e4516defac2b2f40378ba56ede7e026fb0ce5b50
Effene Herry <effene.herry@paper.id>
2021-04-28 22:05:39 +0700

    Fix

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,31 @@ import 'dotenv/config';
 import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync(path.resolve('src/secrets/private-key.pem')),
-    cert: fs.readFileSync(path.resolve('src/secrets/public-certificate.pem')),
-  };
+  if (process.env.NODE_ENV == 'local') {
+    const app = await NestFactory.create(AppModule);
+    app.useWebSocketAdapter(new RedisIoAdapter(app));
+    app.enableCors({
+      origin: true,
+      methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      credentials: true,
+      allowedHeaders:
+        'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,auth',
+    });
+    await app.listen(3000);
+  } else {
+    const httpsOptions = {
+      key: fs.readFileSync(path.resolve('src/secrets/private-key.pem')),
+      cert: fs.readFileSync(path.resolve('src/secrets/public-certificate.pem')),
+    };
 
-  const app = await NestFactory.create(AppModule, {
-   httpsOptions,
-  });
+    const app = await NestFactory.create(AppModule, {
+      httpsOptions,
+    });
 
-  app.useWebSocketAdapter(new RedisIoAdapter(app));
-
-  app.enableCors();
-  await app.listen(3000);
+    app.useWebSocketAdapter(new RedisIoAdapter(app));
+    app.enableCors();
+    await app.listen(3000);
+  }
 }
 
 bootstrap();
