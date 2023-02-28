09a00e6cc33e2702f11ae2840d727b7fc742d554
Damasta Handi Hairullah <damasta.hairullah@gmail.com>
2023-02-28 12:10:26 +0700

    Fix Application Status

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,17 @@ async function bootstrap() {
         'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,auth',
     });
     await app.listen(3000);
+  } else if (process.env.NODE_ENV == 'development') {
+    const app = await NestFactory.create(AppModule);
+    app.useWebSocketAdapter(new RedisIoAdapter(app));
+    app.enableCors({
+      origin: true,
+      methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      credentials: true,
+      allowedHeaders:
+        'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,auth',
+    });
+    await app.listen(3000);
   } else if (process.env.NODE_ENV == 'staging') {
     const app = await NestFactory.create(AppModule);
     app.useWebSocketAdapter(new RedisIoAdapter(app));
@@ -29,6 +40,17 @@ async function bootstrap() {
     });
     await app.listen(3000);
   } else if (process.env.NODE_ENV == 'sandbox') {
+    const app = await NestFactory.create(AppModule);
+    app.useWebSocketAdapter(new RedisIoAdapter(app));
+    app.enableCors({
+      origin: true,
+      methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      credentials: true,
+      allowedHeaders:
+        'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,auth',
+    });
+    await app.listen(3000);
+  } else if (process.env.NODE_ENV == 'production') {
     const app = await NestFactory.create(AppModule);
     app.useWebSocketAdapter(new RedisIoAdapter(app));
     app.enableCors({
