f4a8824367015ae084efd1eee3003a5e2e05d450
Damasta Handi Hairullah <damasta.hairullah@gmail.com>
2023-03-01 12:23:44 +0700

    Fix AppStatus

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,56 @@
 import { NestFactory } from '@nestjs/core';
-import 'dotenv/config';
-import * as fs from 'fs';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './pushNotification/redis-io.adapter';
 const path = require('path');
+import 'dotenv/config';
+import * as fs from 'fs';
 
 async function bootstrap() {
   if (process.env.NODE_ENV == 'local') {
+    const app = await NestFactory.create(AppModule);
+    app.useWebSocketAdapter(new RedisIoAdapter(app));
+    app.enableCors({
+      origin: true,
+      methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      credentials: true,
+      allowedHeaders:
+        'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,auth',
+    });
+    await app.listen(3000);
+  } else if (process.env.NODE_ENV == 'development') {
+    const app = await NestFactory.create(AppModule);
+    app.useWebSocketAdapter(new RedisIoAdapter(app));
+    app.enableCors({
+      origin: true,
+      methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      credentials: true,
+      allowedHeaders:
+        'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,auth',
+    });
+    await app.listen(3000);
+  } else if (process.env.NODE_ENV == 'staging') {
+    const app = await NestFactory.create(AppModule);
+    app.useWebSocketAdapter(new RedisIoAdapter(app));
+    app.enableCors({
+      origin: true,
+      methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      credentials: true,
+      allowedHeaders:
+        'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,auth',
+    });
+    await app.listen(3000);
+  } else if (process.env.NODE_ENV == 'sandbox') {
+    const app = await NestFactory.create(AppModule);
+    app.useWebSocketAdapter(new RedisIoAdapter(app));
+    app.enableCors({
+      origin: true,
+      methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      credentials: true,
+      allowedHeaders:
+        'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,auth',
+    });
+    await app.listen(3000);
+  } else if (process.env.NODE_ENV == 'production') {
     const app = await NestFactory.create(AppModule);
     app.useWebSocketAdapter(new RedisIoAdapter(app));
     app.enableCors({
