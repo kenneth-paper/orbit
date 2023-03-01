8a95516cc4af2955c25d11a68cc283424935b5fd
kenneth-paper <124768257+kenneth-paper@users.noreply.github.com>
2023-03-01 16:18:47 +0700

    reduce cluster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
     exec_mode: 'cluster',
-    instances: 2,
+    instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
